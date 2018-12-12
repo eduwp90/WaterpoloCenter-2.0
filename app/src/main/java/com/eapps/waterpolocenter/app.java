daad587a84336d08d40e038b3cea4afcef08e88e
Edu <eduwp90@gmail.com>
2018-12-12 20:29:17 +0100

    DEC 2018 - Push

--- app/src/main/java/com/eapps/waterpolocenter/app.java
+++ app/src/main/java/com/eapps/waterpolocenter/app.java
@@ -20,9 +20,9 @@ public class app extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-                .applicationId("Jbp3tpUJvfm54iaYts9Q8bcmXR7EUMt3WUmgsQCD")
-                .clientKey("WWQuoaJupwil4fc1PbogqW1M1jFWJXIE2ul3xi3V")
-                .server("https://wpcenter.herokuapp.com/Parse/")
+                .applicationId(getString(R.string.ParseAppID))
+                .clientKey(getString(R.string.ParseClientKey))
+                .server(getString(R.string.ParseServerURL))
 
                 .build()
         );
